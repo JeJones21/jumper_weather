6841f41c34af14f282c91839a6d8c93f501e76b7
Jessica Jones <daddehjones@gmail.com>
2021-11-15 11:42:30 -0500

    change show to index

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
-  def show
+  def index
     location = MapFacade.find_location(params[:location])
     forecast = WeatherFacade.find_weather(location.latitude, location.longitude)
     render json: ForecastSerializer.new(forecast)
