8229734ffe7863aafc2602419c75a4ca684179cf
Jessica Jones <daddehjones@gmail.com>
2021-11-15 12:47:34 -0500

    Create index action

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::ActivitiesController < ApplicationController
   def index
-
+    location = params[:location]
+    forecast = params[:forecast]
+    activities = ActivitiesFacade.things_to_do(location, forecast)
+    render json: ActivitiesSerializer.new(activities)
   end
 end
