1ca569ee677de1394fbf9cfa0d78e00a1a7a9a7f
Jessica Jones <daddehjones@gmail.com>
2021-11-16 13:12:00 -0500

    update names to plural

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::BreweriesController < ApplicationController
     if params[:quantity].to_i > 0
       location = params[:location]
       quantity = (params[:quantity]).to_i
-      breweries = BreweryFacade.find_breweries(location, quantity)
+      breweries = BreweriesFacade.find_breweries(location, quantity)
       render json: BreweriesForecastSerializer.new(breweries)
     else
       render json: {error: "Please enter all required fields"}
