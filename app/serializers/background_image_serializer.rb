88c0adb65e00162b0a1d9192a41b32e938a218dc
Jessica Jones <daddehjones@gmail.com>
2021-11-16 00:07:09 -0500

    create serializer

---
+++ app/serializers/background_image_serializer.rb
@@ -0,0 +1,22 @@
+class BackgroundImageSerializer
+  class << self
+    def new_unsplash(background)
+      {
+        "data": {
+          "type": "image",
+          "id": nil,
+          "attributes": {
+            "image": {
+              "location": background.location,
+              "image_url": background.image_url,
+              "credit": {
+                "source": "Unsplash",
+                "author": background.author
+              }
+            }
+          }
+        }
+      }
+    end 
+  end
+end
