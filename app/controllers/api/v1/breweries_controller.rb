888fe985cbbc9afbed2c8cc6469777ab2a05e924
Jessica Jones <daddehjones@gmail.com>
2021-11-16 13:05:37 -0500

    UPdate controller

---
+++ app/controllers/api/v1/breweries_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::BreweriesController < ApplicationController
+  def index
+    if params[:quantity].to_i > 0
+      location = params[:location]
+      quantity = (params[:quantity]).to_i
+      breweries = BreweryFacade.find_breweries(location, quantity)
+      render json: BreweriesForecastSerializer.new(breweries)
+    else
+      render json: {error: "Please enter all required fields"}
+    end
+  end
+end
