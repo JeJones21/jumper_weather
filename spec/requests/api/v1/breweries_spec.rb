c36cacdab7578d1c2858a3f9429be33e5a8004db
Jessica Jones <daddehjones@gmail.com>
2021-11-16 11:32:24 -0500

    Test request

---
+++ spec/requests/api/v1/breweries_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe '/breweries' do
+  it "can list breweries based off of location and forecast" do
+    get '/api/v1/breweries?location=louisville,ky&quantity=2'
+    expect(response).to be_successful
+
+    brew_info = JSON.parse(response.body, symbolize_names: true)
+
+    expect(brew_info).to be_a(Hash)
+    expect(brew_info[:data]).to be_a(Hash)
+  end
+end
