6e25d7e5a4fd8db8c088f8155be71483fb69ba64
Jessica Jones <daddehjones@gmail.com>
2021-11-16 22:52:10 -0500

    update controller with serializer and conditional

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    user = User
-            .find_by(email: params["user"]["email"])
-            .try(:authenticate, params["user"]["password"])
+    user = User.find_by(email: params["user"]["email"])
+    if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     render json: UserSerializer.new(user)
+    else
+      unauthorized
+    end
   end
 end
